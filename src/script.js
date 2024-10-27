cb06b8f1ae26069d88b6c01ed7ec641978da3ef9
himanshuV09 <whoishimanshuverma@example.com>
2024-10-27 10:40:17 +0530

    adding date feature

--- src/script.js
+++ src/script.js
@@ -4,6 +4,7 @@ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather_icon");
+const date = document.querySelector(".date");
 
 async function checkwheather(city) {
   const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
@@ -13,8 +14,13 @@ async function checkwheather(city) {
   } else {
     var data = await response.json();
     document.querySelector(".city").innerHTML = data.name;
+    const today = new Date();
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    document.querySelector(".date").innerHTML = today.toLocaleDateString(undefined, options);
+
     document.querySelector(".temp").innerHTML =
       Math.round(data.main.temp) + "°c";
+    
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + " km/hr";
 
